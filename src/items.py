5b179c641b1c3f076b035ef162938acc4126429c
Nick <nicholas.zarate@gmail.com>
2020-01-07 15:11:09 -0700

    Create Stock item to hold stock information

--- src/items.py
+++ src/items.py
@@ -7,8 +7,15 @@
 
 import scrapy
 
-
-class StocksScraperItem(scrapy.Item):
+class Stock(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    ticker = scrapy.Field()
+    name = scrapy.Field()
+    intraday_price = scrapy.Field()
+    price_change = scrapy.Field()
+    percent_change = scrapy.Field()
+    volume = scrapy.Field()
+    avg_vol_3_month = scrapy.Field()
+    market_cap = scrapy.Field()
+    pe_ratio_ttm = scrapy.Field()
+    sector = scrapy.Field()
